baa57102e13979a710abb721e1ae77f284dc1580
NajmaddinSamad <gnecmeddin@gmail.com>
2023-04-21 18:38:19 +0400

    car-types section has finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import CarRental from './components/CarRental';
+import CarTypes from './components/CarTypes';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Navbar/>
     <Header/>
     <CarRental/>
+    <CarTypes/>
     
     </>
   );
---
+++ src/components/CarTypes.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './style/CarTypes.css'
+const CarTypes = () => {
+  return (
+    <>    
+    <section className="car-types row">
+    <h2 className="find-car">
+      Find Car by Type
+      </h2>
+      <h6 className="we-offer-type">
+      We offer professional car rental & limousine services in our range of high-end vehicles
+      </h6>
+      <div className="col-12 col-md-4 col-lg-4 type">
+     <div className="for-img-type">
+      <img src='https://www.bmw.co.uk/content/dam/bmw/common/all-models/x-series/x6/2023/highlights/bmw-x-series-x6-sp-desktop.jpg'  />
+      <h3>
+        SUV
+      </h3>
+     </div>
+    </div>  
+    
+      <div className="col-12 col-md-4 col-lg-4 type">
+      <div className="for-img-type">
+      <img src='https://imgd.aeplcdn.com//642x361//n/cw/ec/51330/hyundai-i20-right-front-three-quarter28.jpeg?wm=1&q=75'  />
+      <h3>
+        Couple
+      </h3>
+     </div>
+    </div> 
+    
+       <div className="col-12 col-md-4 col-lg-4 type">
+       <div className="for-img-type">
+      <img src='https://media.ed.edmunds-media.com/hyundai/sonata/2021/oem/2021_hyundai_sonata_sedan_limited_fq_oem_1_1600.jpg'  />
+      <h3>
+        Sedan
+      </h3>
+     </div>
+   
+       </div>
+     </section>
+    
+    </>
+  )
+}
+
+export default CarTypes
