a97ccdb2e9a1e8b781e62f221765f1e3faf1a8b7
NajmaddinSamad <gnecmeddin@gmail.com>
2023-04-26 13:50:56 +0400

    cars page has finished

--- src/components/CarSection.jsx
+++ src/components/CarSection.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './style/Normalize.css'
+import './style/CarSection.css'
+import carData from '../AboutCar.json'
+import CarsCard from './CarsCard'
 
 const CarSection = () => {
   return (
    <>
+   <main className="everything-for-cars row">
+{carData.map(data=>(
+  <CarsCard imgCard1={data.carPhoto1} imgCard2={data.carPhoto2} carName={data.carName} carModel={data.carModel} passengerCount={data.numberOffPassengers} luggageCount={data.luggages} transmission={data.transmission} price={data.price}/>
+))
+}
+   </main>
    
    </>
   )
--- src/components/CarsCard.jsx
+++ src/components/CarsCard.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-
-const CarsCard = () => {
+import React   from 'react'
+import './style/Normalize.css'
+import './style/CarSection.css'
+import { AiFillStar } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
+import { BsFillPersonFill } from "react-icons/bs";
+import { MdLuggage } from "react-icons/md";
+import { TbEngine } from "react-icons/tb";
+const CarsCard = ({imgCard1, imgCard2,carName, carModel,passengerCount, luggageCount, transmission, price }) => {
   return (
   <>
+  <div className="col-12 col-md-6 col-lg-4">
+    <div className="everything-about-cars">
+    <div className="img-cars">
+  <img src={imgCard1} alt="" />
+
+    </div>
+  <div className="down">
+  <div className="about-cars-left">
+    <h3>{carName + "    "}{carModel}</h3>
+      <span>
+        <AiFillStar className='blue-star'/>
+        <AiFillStar className='blue-star'/>
+        <AiFillStar className='blue-star'/>
+        <AiFillStar className='blue-star'/>
+        <AiOutlineStar className='empty-star'/>
+       
+      <h6>4 reviews</h6>
+      </span>
+   <div className="count-luggage">
+   <div className="passenger">
+    <BsFillPersonFill/>
+        <h6>{passengerCount}</h6>
+      </div>
+      <div className="luggage">
+        <MdLuggage/>
+        <h6>{luggageCount}</h6>
+      </div>
+      <div className="transmission">
+        <TbEngine/>
+        <h6>{transmission}</h6>
+      </div>
+   </div>
+    </div>
+    <div className="about-cars-right">
+      <h6>$</h6>
+      <h1>{price}</h1>
+      <h5>Per Day</h5>
+    </div>
+  </div>
+  </div>
   
+    </div>
+   
   </>
   )
 }
