96a0aacd040dae8563c2b3b8580934eab20c58e0
NajmaddinSamad <gnecmeddin@gmail.com>
2023-05-06 17:46:50 +0400

    not found page has finshed

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style/Normalize.css'
+import './style/NotFound.css'
+
+const NotFound = () => {
+  return (
+   <>
+
+ <section className="page-404">
+ <h1>404</h1>
+   <h2>Sorry, Page Not Found <span> 🙄</span></h2>
+   <h5>The page you requested could not be found...</h5>
+   
+ </section>
+   </>
+  )
+}
+
+export default NotFound
